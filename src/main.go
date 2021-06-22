b62ce2e8cbd266aa26155d9c3a8f629fb965562c
mattherix <mattherix@protonmail.com>
2021-06-22 16:12:27 +0200

    Add func getClient and the option to tweet

--- src/main.go
+++ src/main.go
@@ -1,7 +1,70 @@
+// The majority of the code is from https://tutorialedge.net/golang/writing-a-twitter-bot-golang/
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+type Credentials struct {
+	ConsumerKey       string
+	ConsumerSecret    string
+	AccessToken       string
+	AccessTokenSecret string
+}
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Go-Twitter Bot v0.01")
+	creds := Credentials{
+		AccessToken:       os.Getenv("ACCESS_TOKEN"),
+		AccessTokenSecret: os.Getenv("ACCESS_TOKEN_SECRET"),
+		ConsumerKey:       os.Getenv("CONSUMER_KEY"),
+		ConsumerSecret:    os.Getenv("CONSUMER_SECRET"),
+	}
+
+	fmt.Printf("%+v\n", creds)
+	client, err := getClient(&creds)
+	if err != nil {
+		log.Println("Error getting Twitter Client")
+		log.Println(err)
+	}
+
+	fmt.Printf("%+v\n", client)
+
+	println("                                                     ")
+	for i := 0; i < 10; i++ {
+		tweet, resp, err := client.Statuses.Update(fmt.Sprintf("Hello world, just testing the bot in order to #debug #%d", i), nil)
+		if err != nil {
+			log.Println("Error while tweeting")
+			log.Println(err)
+		}
+		log.Printf("%+v\n", resp)
+		log.Printf("%+v\n", tweet)
+	}
+
+}
+
+func getClient(creds *Credentials) (*twitter.Client, error) {
+	config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
+	token := oauth1.NewToken(creds.AccessToken, creds.AccessTokenSecret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	verifyParams := &twitter.AccountVerifyParams{
+		SkipStatus:   twitter.Bool(true),
+		IncludeEmail: twitter.Bool(true),
+	}
+
+	user, _, err := client.Accounts.VerifyCredentials(verifyParams)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("User's ACCOUNT:\n%+v\n", user)
+	return client, nil
 }
